ef85ed7753eee5d1281be8fd2ff865a855fec3cf
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-28 14:42:36 -0700

    printrepair

--- _printf.c
+++ _printf.c
@@ -3,11 +3,11 @@
 #include <stdarg.h>
 
 /**
- * _printf - a function that produces output according to a format
- * @format: character string composed of zero or more directives
- * Return: print out
+ * _printf - produces output according to a format
+ * @format: character string containing zero or more directives
+ * Return: number of characters printed
+ * (excluding the null byte used to end output to strings)
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -17,43 +17,12 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	while (*format)
-
 	{
-	if (*format == '%')
+
+		if (*format == '%')
 	{
 		format++;
-
-		switch (*format)
-		{
-		case 'c':
-
-		{
-			char c = (char) va_arg(args, int);
-
-			putchar(c);
-			count++;
-		}
-		break;
-		case 's':
-
-		{
-			char *str = va_arg(args, char *);
-
-			while (*str)
-
-			{
-				putchar(*str++);
-				count++;
-			}
-		}
-		break;
-		case '%':
-		{
-			putchar('%');
-			count++;
-		}
-		break;
-		}
+		_print_arg(*format, args, &count);
 	}
 	else
 	{
@@ -67,4 +36,44 @@ int _printf(const char *format, ...)
 	va_end(args);
 
 	return (count);
+}
+
+/**
+ * _print_arg - prints a single argument based on the given
+ * conversion specifier
+ * @c: the conversion specifier to use
+ * @args: the argument list to print
+ * @count: pointer to the count of characters printed
+ */
+void _print_arg(char c, va_list args, int *count)
+{
+	switch (c)
+	{
+	case 'c':
+	}
+	char ch = (char) va_arg(args, int);
+
+	putchar(ch);
+	(*count)++;
+	}
+	break;
+
+	case 's':
+	{
+		char *str = va_arg(args, char *);
+
+		while (*str)
+		{
+			putchar(*str++);
+			(*count)++
+		}
+	}
+	break;
+	case '%':
+	{
+		putchar('%');
+		(*count)++;
+	}
+	break;
+	}
 }
