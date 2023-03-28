a4af99d0dc1de17155abebe5d658030d1ae9f103
MadimakatsoM <preciousmadimakatso@gmail.com>
2023-03-29 00:06:52 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -1,79 +1,78 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
+#include "holberton.h"
+
+int (*printer_aux(char flag))(va_list);
 
 /**
- * _printf - produces output according to a format
- * @format: character string containing zero or more directives
- * Return: number of characters printed
- * (excluding the null byte used to end output to strings)
+ * _printf - produces output according to a format.
+ * @format: format specifier.
+ * Return: number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
+	va_list arg;
+	int i, printed_chars = 0;
 
-	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	int count = 0;
-
-	while (*format)
+	va_start(arg, format);
+	for (i = 0; format && format[i]; i++)
 	{
-
-		if (*format == '%')
-	{
-		format++;
-		_print_arg(*format, args, &count);
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			printed_chars++;
+		}
+		else if (format[i + 1] == '\0')
+			return (-1);
+		else if (format[i + 1] == '%')
+		{
+			_putchar(format[i]);
+			printed_chars++;
+			i++;
+		}
+		else if (printer_aux(format[i + 1]) != NULL)
+		{
+			printed_chars = printed_chars + printer_aux(format[i + 1])(arg);
+			i++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			printed_chars++;
+		}
 	}
-	else
-	{
-		putchar(*format);
-		count++;
-	}
+	va_end(arg);
 
-	format++;
-	}
-
-	va_end(args);
-
-	return (count);
+	return (printed_chars);
 }
 
 /**
- * _print_arg - prints a single argument based on the given
- * conversion specifier
- * @c: the conversion specifier to use
- * @args: the argument list to print
- * @count: pointer to the count of characters printed
+ * printer_aux - auxiliar function for print with a specific format.
+ * @flag: format specifier
+ * Return: pointer to format function or NULL.
  */
-void _print_arg(char c, va_list args, int *count)
+int (*printer_aux(char flag))(va_list)
 {
-	switch (c)
-	{
-	case 'c':
-	}
-	char ch = (char) va_arg(args, int);
+	printer_t arr[] = {
+		{'c', print_c},
+		{'s', print_s},
+		{'i', print_i},
+		{'d', print_i},
+		{'b', print_b},
+		{'u', print_u},
+		{'o', print_o},
+		{'x', print_x},
+		{'X', print_X},
+		{'r', print_r},
+		{'R', print_R},
+		{'\0', NULL}
+	};
+	int i;
 
-	putchar(ch);
-	(*count)++;
-	}
-	break;
+	for (i = 0; arr[i].flag != '\0'; i++)
+		if (flag == arr[i].flag)
+			break;
 
-	case 's':
-	{
-		char *str = va_arg(args, char *);
-
-		while (*str)
-		{
-			putchar(*str++);
-			(*count)++
-		}
-	}
-	break;
-	case '%':
-	{
-		putchar('%');
-		(*count)++;
-	}
-	break;
-	}
+	return (arr[i].function);
 }
