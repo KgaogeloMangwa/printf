1225e820425cc2b79849794de5078944218de844
KgaogeloMangwa <kgaogeloextra@gmail.com>
2023-03-28 17:08:17 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,139 +1,37 @@
 #include "main.h"
 
-int (*printer_aux(char flag))(va_list);
-
-void _print_arg(char c, va_list args, int *count);
-
+void print_buffer(char buffer[], int *buff_int);
 /**
- * _printf - produces output according to a format.
- * @format: format specifier.
- * Return: number of characters printed.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-	va_list args;
+	int i, in_length = 0;
+	va_list arg_list;
 
-	int count = 0;
-
-	va_start(args, format);
-
-
-	while (*format)
-	{
-
-		if (*format == '%')
-		{
-			format++;
-			_print_arg(*format, args, &count);
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-
-		format++;
-=======
-	va_list arg;
-	int i, printed_chars = 0;
-
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	va_start(arg_list, format);
+	if (format == NULL)
 		return (-1);
-
-	va_start(arg, format);
-	for (i = 0; format && format[i]; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			_putchar(format[i]);
-			printed_chars++;
-		}
-		else if (format[i + 1] == '\0')
-			return (-1);
-		else if (format[i + 1] == '%')
-		{
-			_putchar(format[i]);
-			printed_chars++;
-			i++;
-		}
-		else if (printer_aux(format[i + 1]) != NULL)
-		{
-			printed_chars = printed_chars + printer_aux(format[i + 1])(arg);
+			if (format[i + 1] == 'c' || format[i + 1] == 's' ||
+			format[i + 1] == 'r' || format[i + 1] == 'R' ||
+			format[i + 1] == '%')
+				in_length = switch_char(arg_list, format[i + 1], in_length);
+			else if (format[i + 1] != '\0')
+				in_length = switch_num(arg_list, format[i + 1], in_length);
 			i++;
 		}
 		else
 		{
 			_putchar(format[i]);
-			printed_chars++;
-		}
->>>>>>> a4af99d0dc1de17155abebe5d658030d1ae9f103
-	}
-	va_end(arg);
-
-	return (printed_chars);
-}
-
-/**
- * printer_aux - auxiliar function for print with a specific format.
- * @flag: format specifier
- * Return: pointer to format function or NULL.
- */
-int (*printer_aux(char flag))(va_list)
-{
-<<<<<<< HEAD
-
-	switch (c)
-	{
-	case 'c':
-		{
-			char ch = (char) va_arg(args, int);
-
-			putchar(ch);
-			(*count)++;
-		}
-		break;
-
-		case 's':
-		{
-			char *str = va_arg(args, char *);
-
-			while (*str)
-		{
-			putchar(*str++);
-			(*count)++;
+			in_length++;
 		}
 	}
-	break;
-	case '%':
-	{
-		putchar('%');
-		(*count)++;
-	}
-	break;
-	}
-=======
-	printer_t arr[] = {
-		{'c', print_c},
-		{'s', print_s},
-		{'i', print_i},
-		{'d', print_i},
-		{'b', print_b},
-		{'u', print_u},
-		{'o', print_o},
-		{'x', print_x},
-		{'X', print_X},
-		{'r', print_r},
-		{'R', print_R},
-		{'\0', NULL}
-	};
-	int i;
-
-	for (i = 0; arr[i].flag != '\0'; i++)
-		if (flag == arr[i].flag)
-			break;
-
-	return (arr[i].function);
->>>>>>> a4af99d0dc1de17155abebe5d658030d1ae9f103
+	va_end(arg_list);
+	return (in_length);
 }
--- main.h
+++ main.h
@@ -29,4 +29,18 @@ int print_x(va_list arg);
 int print_X(va_list arg);
 int print_r(va_list arg);
 int print_R(va_list arg);
+
+int switch_num(va_list, char, int);
+int switch_char(va_list, char, int);
+int print_char(va_list, int *);
+int print_string(va_list, int *, char);
+int print_num(va_list, int *, char);
+char *num_converter(unsigned int, unsigned int *);
+char *hex_converter(unsigned int, unsigned int *, char);
+int count_digits(unsigned int, unsigned int *);
+int out_num(char *, int, int *);
+char *bnten(int, char, unsigned int *);
+void rev_string(char *, int);
+char *rot13(char *, int);
+
 #endif
